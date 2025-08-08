4adc32f2f49e60b2d589a08e5af9c9a937997624
neetibut <neetibut@gmail.com>
2025-08-08 12:05:10 +0700

    feat(view): rounded img style update

--- src/views/Owner.jsx
+++ src/views/Owner.jsx
@@ -5,7 +5,7 @@ export default function Owner() {
       <img
         src="https://upload.wikimedia.org/wikipedia/en/d/de/BSG_-_William_Adama_%28Edward_James_Olmos%29.jpg"
         alt="user image"
-        className="w-64"
+        className="w-64 rounded-2xl"
       />
       <p className="text-center">
         <span className="font-bold">Short Bio:</span>
