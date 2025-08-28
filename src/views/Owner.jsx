4b7b67c30d16c8ea29dfa0d142a475bb11fe118b
jittawee <jtwe.esr@gmail.com>
2025-08-28 09:51:17 +0700

    second commit

--- src/views/Owner.jsx
+++ src/views/Owner.jsx
@@ -1,19 +1,16 @@
 export default function Owner() {
   return (
     <div className="min-h-screen flex flex-col items-center pt-12 gap-y-3 w-1/2">
-      <h1 className="text-4xl font-bold text-center">99 Instructor - JSDX</h1>
+      <h1 className="text-4xl font-bold text-center">08 Jittawee (Arm/อาร์ม) - JSD 10</h1>
       <img
-        src="https://upload.wikimedia.org/wikipedia/en/d/de/BSG_-_William_Adama_%28Edward_James_Olmos%29.jpg"
-        alt="user image"
+        src="owner.jpg"
+        alt="owner image"
         className="w-64 rounded-2xl"
       />
       <p className="text-center">
         <span className="font-bold">Short Bio:</span>
         <br />
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-        aliquid, reiciendis quisquam error, delectus odit minima hic porro sequi
-        voluptatem dolores? Similique quia totam ad ratione quas animi facilis
-        ipsum!
+        Motivated career shifter currently training as a Junior Software Developer with a strong foundation in communication, documentation, and process management. Drawing from experience as an Executive Assistant, ISO/IEC 27001 Quality Assurance, and BA Support/Translator, I bring proven skills in cross-functional collaboration, analytical thinking, and attention to detail. I have coordinated with technical teams on IT compliance, prepared and translated project documentation, supported Agile workflows, and maintained accuracy under pressure.
       </p>
     </div>
   );
